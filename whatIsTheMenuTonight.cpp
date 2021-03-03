2bb0363dca8c85e596a0f6450fc4dbaae45eb98c
Cyan75 <incanto075@gmail.com>
2021-03-03 23:58:54 +0900

    first successful build. sanity check and memory allocation adjustment needed

--- whatIsTheMenuTonight.cpp
+++ whatIsTheMenuTonight.cpp
@@ -6,48 +6,44 @@ class RandomNumberSelector
 {
 public:
     short maximum;
-    RandomNumberSelector(short defaultMax = 1) : maximum(defaultMax) {}
+    RandomNumberSelector(void) //generate seed
+    {
+        std::srand(std::time(nullptr));
+        randSeed = std::rand();
+    }
     virtual void setMax(short passMax)
     {
         maximum = passMax;
     }
-    short getRandomNum(void)
+    short getRanNum(void) //random generate
     {
-        short generateRandNum = randomNumberGenerator();
-        return generateRandNum;
+        return (randSeed % maximum);
     }
 
 private:
-    short randomNumberGenerator(void)
-    {
-        /* RNG code */
-        std::srand(std::time(nullptr)); //seed
-        short randomNumber = (std::rand() % maximum);
-        return randomNumber;
-    }
+    short randSeed;
 };
 class RandomMenuSelector : public RandomNumberSelector
 {
 public:
-    RandomMenuSelector(std::string inputFileName)
+    RandomMenuSelector(std::string inputFileName) : RandomNumberSelector::RandomNumberSelector()
     {
         std::ifstream listFile(inputFileName);
         std::string line;
+        short numLines = 0;
         if (listFile.is_open())
         {
             while (std::getline(listFile, line))
             {
                 list.push_back(line);
+                numLines++;
             }
+            maximumMenu = numLines;
         }
     }
-    virtual void setMax(short numElements = 0)
+    virtual void setMax(void)
     {
-        for (std::vector<std::string>::iterator it = list.begin(); it != list.end(); ++it)
-        {
-            ++numElements;
-        }
-        maximum = numElements;
+        maximum = maximumMenu;
     }
     void printList(void)
     {
@@ -56,13 +52,19 @@ public:
             std::cout << *it << std::endl;
         }
     }
+    std::string getRanMenu(void)
+    {
+        std::vector<std::string>::iterator it = list.begin();
+        return (*(it+getRanNum()));
+    }
 private:
     std::vector<std::string> list;
+    short maximumMenu;
 };
 int main(void)
 {
-    RandomMenuSelector rm1("betterRestBistList.txt");
-    rm1.printList();
-    //RandomNumberSelector r1(rm1.getLengthPrintList());
+    RandomMenuSelector rms1("normalRestBistList.txt");
+    rms1.setMax();
+    std::cout<<rms1.getRanMenu();
     return 0;
 }
