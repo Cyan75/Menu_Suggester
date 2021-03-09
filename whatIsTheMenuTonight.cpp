8f773b38d298ffbb6d2aec46f4acf5aa96e4c743
Cyan75 <incanto075@gmail.com>
2021-03-09 23:31:47 +0900

    all in-code comments removed

--- whatIsTheMenuTonight.cpp
+++ whatIsTheMenuTonight.cpp
@@ -6,43 +6,29 @@ class RandomNumberSelector
 {
 public:
     short maximum;
-    RandomNumberSelector(void) //generate seed as soon as when class is instantiated
+    RandomNumberSelector(void)
     {
         std::srand(std::time(nullptr));
         randSeed = std::rand();
     }
-    //most general form of setMax is to assign the passed value
-    //setMax can be redefined
-    /* 
-    how about using pure virtual
-    virtual void setMAx = 0; 
-    */
     virtual void setMax(short &passMax)
     {
         maximum = passMax;
     }
-    short getRanNum(void) //RNG
+    short getRanNum(void)
     {
         return (randSeed % maximum);
     }
-
 private:
     short randSeed;
 };
 class RandomMenuSelector : public RandomNumberSelector
 {
 public:
-    //a constructor of a derived class should also be inherited from the base class constructor
-    //RMS needs to take strings from a txt file
-    //RandomMenuSelector(std::string inputFileName) : RandomNumberSelector::RandomNumberSelector()
     RandomMenuSelector(void) : RandomNumberSelector::RandomNumberSelector()
     {
-        // what is it to use dynamic allocation in the constructor?
-        // dynamic: what for?
-        // is it always good to allocate dynamically?
         std::string inputFileName = selectMenuType();
         std::ifstream listFile(inputFileName);
-        //std::ifstream listFile(selectMenuType());
         std::string line;
         short numLines = 0;
         if (listFile.is_open())
@@ -87,18 +73,12 @@ public:
                 break;
             }
         } while (isValid == false);
-
         return menuListName;
     }
-    /* ~RandomMenuSelector()
-    {
-        delete maximumMenu;
-    } */
     void setMax(void)
     {
         maximum = maximumMenu;
     }
-    //redundant -> just for debugging
     void printList(void)
     {
         for (std::vector<std::string>::iterator it = list.begin(); it != list.end(); ++it)
@@ -115,7 +95,6 @@ public:
     {
         list.empty();
     }
-
 private:
     std::vector<std::string> list;
     short maximumMenu;
